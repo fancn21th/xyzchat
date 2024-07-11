299e6be257a320e1fceddf41fb5961fc61573772
fancn21th <fancn21th@gmail.com>
2024-07-11 16:32:56 +0800

    👌 IMPROVE: remove demo stories

--- apps/design-system/.storybook/main.ts
+++ apps/design-system/.storybook/main.ts
@@ -15,7 +15,7 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
+    // getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
