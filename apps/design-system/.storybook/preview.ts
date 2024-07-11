a9dc97ac9842e5d327690789938d9d539bb53c50
fancn21th <fancn21th@gmail.com>
2024-07-11 17:23:04 +0800

    📦 NEW: light & dark theme

--- apps/design-system/.storybook/preview.ts
+++ apps/design-system/.storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes";
+// import { withThemeByDataAttribute } from "@storybook/addon-themes";
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +11,23 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+    // withThemeByDataAttribute({
+    //   defaultTheme: "light",
+    //   themes: {
+    //     light: "light",
+    //     dark: "dark",
+    //   },
+    //   attributeName: "data-mode",
+    // }),
+  ],
 };
 
 export default preview;
