165a81d4324d34d22ead1f46fa947d594906cbaa
fancn21th <fancn21th@gmail.com>
2024-07-11 15:54:19 +0800

    📦 NEW: import tailwind css in theme provider way

--- apps/design-system/stories/Chat.stories.tsx
+++ apps/design-system/stories/Chat.stories.tsx
@@ -1,13 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Chat } from "@repo/core";
+import { Chat, ThemeProvider } from "@repo/core";
 
-const meta = {
+const meta: Meta = {
   title: "Components/Chat",
   component: Chat,
   args: {
     children: "Chat",
   },
   argTypes: {},
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
 } satisfies Meta;
 
 export default meta;
