0a17f6f97497ada59dacdef8d8b994b12ef136ac
fancn21th <fancn21th@gmail.com>
2024-08-25 16:27:24 +0800

    📦 NEW: ai-sdk sample

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,9 +1,24 @@
-import { ThemeProvider, Chat } from "@xyzchat/components";
+import { ThemeProvider } from "@xyzchat/components";
+import { useCompletion } from "ai/react";
 
 function App() {
+  const { completion, input, handleInputChange, handleSubmit } = useCompletion({
+    api: "/mock/api/completion",
+    onResponse: () => {},
+  });
+
   return (
     <ThemeProvider>
-      <Chat>Hello xyzChat</Chat>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="prompt"
+          value={input}
+          onChange={handleInputChange}
+          id="input"
+        />
+        <button type="submit">Submit</button>
+        <div>{completion}</div>
+      </form>
     </ThemeProvider>
   );
 }
