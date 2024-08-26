420a994660a08ccd24420b57ccf23cf9affbc5a2
fancn21th <fancn21th@gmail.com>
2024-08-26 18:06:21 +0800

    📦 NEW: add styles to the sample

--- apps/mobile/src/app/page.tsx
+++ apps/mobile/src/app/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import { useCompletion } from "ai/react";
+import { ThemeProvider, HumanInput } from "@xyzchat/components";
 
 export default function Chat() {
   const { completion, input, handleInputChange, handleSubmit } =
     useCompletion();
 
   return (
-    <div>
-      {completion}
-      <form onSubmit={handleSubmit}>
-        <input value={input} onChange={handleInputChange} />
-      </form>
-    </div>
+    <ThemeProvider>
+      <div className="w-screen h-screen p-8 flex justify-center items-center flex-rows">
+        <div className="w-full h-full border rounded-lg relative">
+          <div className="p-8">
+            <div className="bg-slate-500 text-white p-8 rounded-lg">
+              {completion}
+            </div>
+          </div>
+          <div className="absolute bottom-0 w-full p-8">
+            <form onSubmit={handleSubmit}>
+              <HumanInput value={input} onChange={handleInputChange} />
+            </form>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
