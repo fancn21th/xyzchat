7944140979804079a9767d98803cdfa385c834fb
fancn21th <fancn21th@gmail.com>
2024-10-18 14:34:54 +0800

    📦 NEW: path update

--- packages/core/stories/helpers/GridBackground.stories.tsx
+++ packages/core/stories/helpers/GridBackground.stories.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { GridBackground } from "../../src/components";
+import { GridBackground } from "@/components";
 
 const meta: Meta = {
   title: "Helpers/GridBackground",
