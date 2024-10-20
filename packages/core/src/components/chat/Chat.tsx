c62b1e2018f4993c4f77ee7d7cda2b9d8f4b4582
fancn21th <fancn21th@gmail.com>
2024-10-20 12:44:51 +0800

    📦 NEW: removed over designed

--- packages/core/src/components/chat/Chat.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentProps } from "react";
-
-import { Button, type ButtonProps } from "../ui/button";
-
-export type ChatProps = ComponentProps<"button"> & ButtonProps;
-
-export function Chat(props: ChatProps): JSX.Element {
-  return <Button {...props} />;
-}
