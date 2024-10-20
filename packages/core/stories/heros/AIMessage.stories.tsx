710e909a5844b13f65c35f9b1f4b9a592524a338
fancn21th <fancn21th@gmail.com>
2024-10-20 12:23:05 +0800

    📦 NEW: the three heros

---
+++ packages/core/stories/heros/AIMessage.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { AIMessage } from "@/components";
+
+const meta: Meta = {
+  title: "Components/AIMessage",
+  component: AIMessage,
+  args: {
+    children: "我是一个机器人",
+  },
+  argTypes: {},
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof AIMessage>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ packages/core/stories/heros/HumanMessage.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { HumanMessage } from "@/components";
+
+const meta: Meta = {
+  title: "Components/HumanMessage",
+  component: HumanMessage,
+  args: {
+    children: "我是一个人类",
+  },
+  argTypes: {},
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof HumanMessage>;
+
+export const Primary: Story = {
+  args: {},
+};
